ce66b9699c24df01e70c88193c06f8ccf32da9ad
HiImZsofi <zsofia.gyori00@gmail.com>
2022-10-11 12:00:57 +0200

    masodik feladat

--- task.js
+++ task.js
@@ -1,14 +1,17 @@
 const numberarray = Array(20).fill().map(() => Math.floor(30 * Math.random() + 1) );
  const dividedNumbers = []
- 
+
 //tiszta ig
 function divideArray(array){
     dividedNumbers.push(array.filter(array => array % 5 == 0)) 
     return dividedNumbers
 }
 
-/*document.addEventListener('DOMContentLoaded', () => {
-    
-})*/
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('divide').addEventListener('click', () => {
+        console.log(divideArray(numberarray))
+    })
+})
 
-console.log(divideArray(numberarray))
+//console.log(divideArray(numberarray))
+
